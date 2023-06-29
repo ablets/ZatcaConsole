44833e10ff7fbc0959e4c6dfe50977628fb12f51
Able <ablets214@outlook.com>
2023-06-30 01:29:27 +0530

    Add project files.

---
+++ ZatcaConsole/InvoiceHasService.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace ZatcaInvoiceHash;
+
+public class InvoiceHashService
+{
+    public static string GenerateInvoiceHash(string xmlFilePath, string? fatooraPath)
+    {
+        if (!File.Exists(xmlFilePath))
+        {
+            Console.WriteLine($"Could not find the file {xmlFilePath}");
+        }
+
+        if (string.IsNullOrEmpty(fatooraPath))
+        {
+            fatooraPath = Environment.GetEnvironmentVariable("FATOORA_HOME", EnvironmentVariableTarget.User);
+        }
+
+        if (string.IsNullOrEmpty(fatooraPath))
+        {
+            throw new ArgumentNullException("FATOORA_HOME", "Environment variable is not set");
+        }
+
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "cmd.exe",
+            Arguments = $"/c fatoora -generateHash -invoice \"{xmlFilePath}\"",
+            WorkingDirectory = fatooraPath,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        Process process = new()
+        {
+            StartInfo = startInfo
+        };
+        process.Start();
+        process.WaitForExit();
+
+        string result = process.StandardOutput.ReadToEnd();
+
+        if (result.Contains("invoice hash has been generated successfully"))
+        {
+            string invoiceHashText = "INVOICE HASH = ";
+            int invoiceHashStartIndex = result.IndexOf(invoiceHashText) + invoiceHashText.Length;
+            string invoiceHash = result.Substring(invoiceHashStartIndex).Trim();
+
+            return invoiceHash;
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ ZatcaConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using ZatcaInvoiceHash;
+
+do
+{
+    Console.WriteLine("\nProvide an absolute file path of the xml\n");
+    string? xmlFilePath = Console.ReadLine();
+    if (string.IsNullOrEmpty(xmlFilePath))
+    {
+        xmlFilePath = Path.Combine(Environment.CurrentDirectory, "xml/sample.xml");
+        Console.WriteLine($"Using example file: {xmlFilePath} ");
+
+    }
+
+    xmlFilePath = xmlFilePath.Replace("\"", "").Replace("'", "").Trim();
+    bool isAbsolutePath = Path.IsPathRooted(xmlFilePath); // Returns true
+
+    if (!isAbsolutePath)
+    {
+        Console.WriteLine("Not an absolute path");
+    }
+
+    string fatooraPath = @"";
+    string invoiceHash = InvoiceHashService.GenerateInvoiceHash(xmlFilePath, fatooraPath);
+    if(string.IsNullOrEmpty(invoiceHash))
+    {
+        Console.WriteLine("Could not generate invoice hash");
+    }
+    else
+    {
+        Console.WriteLine($"Invoice hash: {invoiceHash}");
+    }
+}
+while
+(true);
+
+
